ffb51237c974626ea3a2850c488de50e360dec80
kering <1074741118@qq.com>
2018-10-15 10:11:14 +0800

    重置部分类名称；重置部分参数名称；新增失败返回值参数支持；

--- ezretry/__init__.py
+++ ezretry/__init__.py
@@ -1,4 +1,4 @@
 from ezretry.api import retry, retry_call
-from ezretry.core import RetryParamItem, EzRetryException
+from ezretry.core import RetryGroup, RetryException
 
-__version__ = '0.0.2'
+__version__ = '1.0.0'
